3d085f5430bbebddd67261cfc990653f6e04762e
toj <77548752+KakarotWasTaken@users.noreply.github.com>
2026-01-14 16:11:23 -0500

    Update AmmtojBalGameWithCommentsSummative1.java

--- Labs/AmmtojBalGameWithCommentsSummative1.java
+++ Labs/AmmtojBalGameWithCommentsSummative1.java
@@ -24,8 +24,22 @@ public class AmmtojBalGameWithCommentsSummative1
     System.out.println(" By: Ammtoj Bal");
     System.out.println("=======================================");
     System.out.println();
-    System.out.println("This is a text-based escape room game. Use the menu below to proceed.");
+    System.out.println("This is a text-based escape room game. Use the menu below to proceed (Appears in 5 seconds).");
     System.out.println();
+    // Clear the introduction screen, using try, catch and thread, found online at https://www.geeksforgeeks.org/java/thread-sleep-method-in-java-with-examples/
+    try 
+    {
+      Thread.sleep(5000); // Wait for 5 seconds before clearing
+    } 
+    catch (InterruptedException e) 
+    {
+      e.printStackTrace(); // Handle the exception (in case the thread gets interrupted)
+    }
+    // Print 15 new lines to simulate clearing the screen
+    for (int i = 0; i < 15; i++) 
+    {
+      System.out.println();
+    }
 
     // Main menu loop
     boolean keepRunning = true;
